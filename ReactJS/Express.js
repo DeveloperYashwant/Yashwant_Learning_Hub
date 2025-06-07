aa816e15b651a6e225de337a09647ddb1425e1d7
unknown <yashghole12@gmail.com>
2025-06-07 21:24:53 +0530

    Initial commit

---
+++ ReactJS/Express.js
@@ -0,0 +1,87 @@
+
+### **Express.js **
+
+---
+
+#### **Lecture 1: Introduction to Express.js**
+
+* What is Express.js?
+* Why use Express with Node.js?
+* Installing Express using `npm`
+* Creating your first Express app
+
+---
+
+#### **Lecture 2: Basic Routing**
+
+* What are routes?
+* Creating routes: GET, POST
+* Route structure (`app.get`, `app.post`)
+* Sending text and HTML response
+
+---
+
+#### **Lecture 3: Handling Request and Response**
+
+* What is `req` (request) and `res` (response)?
+* Accessing request data (params, query)
+* Sending JSON response
+
+---
+
+#### **Lecture 4: Middleware in Express**
+
+* What is middleware?
+* Application-level middleware
+* Using `next()` function
+* Creating custom middleware
+
+---
+
+#### **Lecture 5: Serving Static Files**
+
+* What are static files (HTML, CSS, images)?
+* Using `express.static()`
+* Creating a public folder
+
+---
+
+#### **Lecture 6: Express with HTML Forms**
+
+* Creating a simple HTML form
+* Handling form data using POST
+* `body-parser` middleware (or built-in `express.urlencoded()`)
+
+---
+
+#### **Lecture 7: Route Parameters and Query Strings**
+
+* What are route parameters (`/user/:id`)?
+* Accessing dynamic values from URL
+* Understanding query strings (`?name=abc`)
+
+---
+
+#### **Lecture 8: REST API with Express**
+
+* What is an API?
+* Creating simple REST routes: GET, POST, PUT, DELETE
+* CRUD example with dummy data
+
+---
+
+#### **Lecture 9: Using JSON Files as Database**
+
+* Storing data in JSON file
+* Reading and writing JSON file using `fs` module
+* Connecting it with Express routes
+
+---
+
+#### **Lecture 10: Mini Project**
+
+* Example: Contact Manager or Student Info API
+* Form to add data
+* Displaying data from JSON
+* Deleting data by ID
+
---
+++ ReactJS/Node.js
@@ -0,0 +1,86 @@
+
+### **Node.js **
+
+#### **Lecture 1:** Introduction to Node.js
+
+* What is Node.js?
+* Features and uses of Node.js
+* Installing Node.js
+* Writing and running your first program (`console.log`)
+
+---
+
+#### **Lecture 2:** Understanding Node.js Basics
+
+* Node.js as a JavaScript runtime
+* Node.js vs JavaScript in browser
+* REPL (Read-Eval-Print-Loop) basics
+* Running `.js` files from terminal
+
+---
+
+#### **Lecture 3:** Node.js Modules
+
+* What are modules?
+* Built-in modules (`fs`, `path`, `os`)
+* Creating and using your own module
+* Using `require` and `exports`
+
+---
+
+#### **Lecture 4:** File System Module (`fs`)
+
+* Reading files
+* Writing files
+* Updating and deleting files
+* Synchronous vs asynchronous methods
+
+---
+
+#### **Lecture 5:** Node.js Package Manager (npm)
+
+* What is npm?
+* Installing packages
+* Creating `package.json`
+* Installing and using external modules
+
+---
+
+#### **Lecture 6:** Events and EventEmitter
+
+* What are events?
+* Creating and listening to custom events
+* EventEmitter class basics
+
+---
+
+#### **Lecture 7:** Creating a Basic Server
+
+* Introduction to `http` module
+* Creating a basic web server
+* Sending simple HTML content
+
+---
+
+#### **Lecture 8:** Handling Routes
+
+* Understanding URLs and routes
+* Creating multiple routes (Home, About, Contact)
+* Sending different responses for different routes
+
+---
+
+#### **Lecture 9:** Working with JSON and API Basics
+
+* What is JSON?
+* Creating and using JSON data
+* Introduction to API and REST basics
+
+---
+
+#### **Lecture 10:** Real-Life Mini Project
+
+* Example: Student Info API or Simple Contact Book
+* Read/write JSON file data
+* Display data via a Node.js server
+
